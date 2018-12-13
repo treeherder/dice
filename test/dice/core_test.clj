d473520396719768e81b6e39554e583053499c14
Fiver <brendan@packetfire.org>
2018-12-12 18:40:05 -0600

    Building tests for regex.
    Signed-off-by: Fiver <brendan@packetfire.org>

--- test/dice/core_test.clj
+++ test/dice/core_test.clj
@@ -2,7 +2,7 @@
   (:require [clojure.test :refer :all]
             [dice.core :refer :all]))
 
-(deftest a-test
-  (testing "Get the number and type of dice."
-    (is clojure.lang.PersistentVector
+(deftest match_is_vec
+  (testing "That the product of phrase \"2d20\" is a Persistent Vector."
+  (is clojure.lang.PersistentVector
         ((fn numtyp  [x] (re-find (re-pattern "([1-9]\\d*)?d([1-9]\\d*)") x))"2d20"))))
